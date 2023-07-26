9c93b99551637147532df0fab8380975168ed431
2NG CODER <nahomnigussie7@gmail.com>
2023-07-26 10:56:52 -0700

    AirBnB clone - Web framework tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-listening on 0.0.0.0, port 5000
-"""
-from flask import Flask, render_template
-from models.state import State
-from models import storage
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
+
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown(self):
-    """Function to remove the current SQLAlchemy Session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Function that displays States"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Function that displays Cities by States"""
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    sc = []
-    for state in states:
-        sc.append([state, sorted(state.cities, key=lambda k: k.name)])
-    return render_template('8-cities_by_states.html', states=sc, h1="States")
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
